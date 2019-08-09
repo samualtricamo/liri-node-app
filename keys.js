fa573a4773e37c094512f1ba29edf77f86195984
Sam Tricamo <samualtricamo@gmail.com>
2019-08-09 16:11:31 -0500

    added spotify api keys

--- keys.js
+++ keys.js
@@ -5,3 +5,10 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+
+
+
+//spotify information//
+// Client ID f2a268591b024c6f84c31a866af81dc9//
+// Client Secret 7a975592f0164122aa97b5d9ea7700fb//
--- liri.js
+++ liri.js
@@ -1,10 +1,10 @@
 require(something.env).config();
 
 var request = require("request");
-var fs = require("fs");
-var keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
+var fs = require();
+var keys = require("./keys");
+var Spotify = require();
+var spotify = new Spotify();
 //vars to capture user inputs.
 var userOption = process.argv[2]; 
 var inputParameter = process.argv[3];
